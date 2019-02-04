84c4cfcd9a3df3ab2c586ae8ba95690871dbdac7
Alex Chan <alex@alexwlchan.net>
2019-02-04 07:33:34 +0000

    Fix a deprecation warning

--- src/_plugins/create_idea_hierarchy.rb
+++ src/_plugins/create_idea_hierarchy.rb
@@ -7,6 +7,7 @@ module IdeaHierarchy
       # TODO: Check that every idea is used exactly once.
       site.data["idea_categories"] = categories.map { |category|
         category["ideas"] = ideas
+          .docs
           .select { |idea| idea.data["category"] == category["id"] }
           .sort_by { |idea| idea["title"] }
         category
